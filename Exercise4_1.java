0c6bf8c4a51d52e46e492ac57f013c6704a5d1a9
BirgulGokturk <250541094@firat.edu.tr>
2025-11-30 19:30:16 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,45 @@
+/*
+ * Ad Soyad: Birgül Göktürk
+ * Ogrenci No:250541094
+ * Tarih:30.11.2025
+ * 
+
+import java.util.Scanner;
+
 public class Exercise4_1 {
 
+    // Amerikan Formatı
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    // Avrupa Formatı
+    public static void printEuropean(String gun, int tarih, String ay, int yil) {
+        System.out.println(tarih + " " + ay + " " + yil + ", " + gun);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Günü giriniz (örn: Pazartesi): ");
+        String gun = input.nextLine();
+
+        System.out.print("Ayı giriniz (örn: Temmuz): ");
+        String ay = input.nextLine();
+
+        System.out.print("Tarihi giriniz (örn: 22): ");
+        int tarih = input.nextInt();
+
+        System.out.print("Yılı giriniz (örn: 2019): ");
+        int yil = input.nextInt();
+
+        System.out.println();
+        System.out.println("=== Amerikan Formatı ===");
+        printAmerican(gun, tarih, ay, yil);
+
+        System.out.println("=== Avrupa Formatıs ===");
+        printEuropean(gun, tarih, ay, yil);
     }
 }
+
+ 
